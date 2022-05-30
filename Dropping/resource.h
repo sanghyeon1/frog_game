b8a479badc616dfdaf7196a60991f617a8416454
shinmin9812 <shinmin9812@naver.com>
2022-05-30 18:15:55 +0900

    초기 커밋.

---
+++ Dropping/resource.h
@@ -0,0 +1,54 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// Dropping.rc에서 사용되고 있습니다.
+//
+#define IDC_MYICON                      2
+#define IDD_DROPPING_DIALOG             102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_DROPPING                    107
+#define IDI_SMALL                       108
+#define IDC_DROPPING                    109
+#define IDR_MAINFRAME                   128
+#define IDB_BITMAP_ALCHOL               129
+#define IDB_BITMAP_BAT                  130
+#define IDB_BITMAP_CRICKET              131
+#define IDB_BITMAP_DNA                  132
+#define IDB_BITMAP_DOPPING              133
+#define IDB_BITMAP_FLY                  134
+#define IDB_BITMAP_ROCK                 135
+#define IDB_BITMAP_SLIME                136
+#define IDB_BITMAP_SNAKE                137
+#define IDB_BITMAP_WORM                 138
+#define IDB_BITMAP_GAMEOVER             139
+#define IDB_BITMAP_L13                  140
+#define IDB_BITMAP_L46                  141
+#define IDB_BITMAP_L79                  142
+#define IDB_BITMAP_L1012                143
+#define IDB_BITMAP_ML1                  144
+#define IDB_BITMAP_MR1                  145
+#define IDB_BITMAP_ML2                  146
+#define IDB_BITMAP_MR2                  147
+#define IDB_BITMAP_ML3                  148
+#define IDB_BITMAP_MR3                  149
+#define IDB_BITMAP_TL1                  150
+#define IDB_BITMAP_TR1                  151
+#define IDB_BITMAP_TL2                  152
+#define IDB_BITMAP_TR2                  153
+#define IDB_BITMAP_TL3                  154
+#define IDB_BITMAP_TR3                  155
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        156
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
